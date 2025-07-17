7d57c92508960e8873bdeb726dc39a72b71e57b7
Harshit Singh <harshit.x.singh@gmail.com>
2025-07-18 00:39:33 +0530

    fead : [added the week 4]

---
+++ Week5_Microservices_with_API_gateway/2_Create_Eureka_Discovery_Server_and_register/code/apigateway/src/main/java/com/cognizant/apigateway/filter/LoggingGlobalFilter.java
@@ -0,0 +1,23 @@
+package com.cognizant.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class LoggingGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        logger.info("Path of the request received -> {}", request.getPath());
+        return chain.filter(exchange);
+    }
+}
